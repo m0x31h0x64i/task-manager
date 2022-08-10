647f4c056bb5168727fc16954b554e2c17dc5162
m0x31h0x64i <m0x31h0x64i@gmail.com>
2022-08-09 21:46:19 -0400

    new file name

---
+++ email/mailer.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS
+    }
+})
+
+const sendWelcomeEmail = (email, name) => {
+    transporter.sendMail({
+        to: email,
+        subject: 'Welcome to task-manager app!',
+        html: `Hello <b>${name}</b>👋🏻. If things went wrong let us know.
+        <img src="cid:logo">`,
+        attachments: [{
+          path: __dirname + '/resource/welcome.jpg',
+          cid: 'logo'
+      }]
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    transporter.sendMail({
+      to: email,
+      subject: 'Good bye!',
+      html: `Hello <b>${name}</b>. sorry to see you go🥲
+        <img src="cid:logo">`,
+      attachments: [{
+        path: __dirname + '/resource/goodbye.webp',
+        cid: 'logo'
+      }]
+    })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail
+}
